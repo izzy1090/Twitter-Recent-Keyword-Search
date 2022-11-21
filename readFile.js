479a0afed6a304fdf45740c71e895410afea23fd
izzy1090 <michaelisabella1@gmail.com>
2022-11-21 14:58:23 -0500

    Created directory and turned funcs. into scripts
    Created a working directory to allow for individual scripts to
    host isolated functions. This makes the program's overall flow cleaner
    and easier to understand.
    
    Flow is subject to change in future commits.

---
+++ readFile.js
@@ -0,0 +1,24 @@
+// dependencies needed to run script
+const fs = require('fs')
+const {readFileSync, promises: fsPromises} = require('fs');
+
+// function declaration asyncReadFile to read in a file 
+    // and convert contents from file into an array of objects
+// input: text file 
+// output: array
+const asyncReadFile = async (filename) => {
+    try {
+        // awaits promise for 'fs' to read in file
+        const contents = await fsPromises.readFile(filename, 'utf-8');
+        // then parse it out to an array of objects
+        const backToJSON = JSON.parse(contents)
+        // then returns it back to func
+        return backToJSON;
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+module.exports = {
+    asyncReadFile
+}
